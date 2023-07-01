64c0eb5845ada453a1aee14ef8f2450ede8b820f
nisala1997 <s18326@sci.pdn.ac.lk>
2023-07-01 23:41:54 +0530

    Initial commit

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,173 @@
+import React, {useEffect}  from 'react'
+import './main.css'
+import img from '../../Assets/img.jpg'
+import img2 from '../../Assets/img2.jpg'
+import img3 from '../../Assets/img3.jpg'
+import img4 from '../../Assets/img4.jpg'
+import img5 from '../../Assets/img5.jpg'
+import img6 from '../../Assets/img6.jpg'
+import img7 from '../../Assets/img7.jpg'
+import img8 from '../../Assets/img8.jpg'
+import img9 from '../../Assets/img9.jpg'
+
+import {HiOutlineLocationMarker} from 'react-icons/hi'
+import {HiOutlineClipboardCheck} from 'react-icons/hi'
+
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+//array named data
+
+const Data = [
+  {
+    id:1,
+    imgSrc: img,
+    destTitle: 'Bora Bora',
+    location: 'New Zealand',
+    grade: 'CULTURAL RELAX',
+    fees: '$700',
+    description: 'The epitome of romance, Bora is one of the best travel destination in the world.This place is know for its luxurious stays and adventurous activities.'
+  },
+
+  {
+    id:2,
+    imgSrc: img2,
+    destTitle: 'Machu Picchu',
+    location: 'Peru',
+    grade: 'CULTURAL RELAX',
+    fees: '$600',
+    description: 'Huayna picchu is a.'
+  },
+
+  {
+    id:3,
+    imgSrc: img3,
+    destTitle: 'Great Barrier Reef',
+    location: 'Australia',
+    grade: 'CULTURAL RELAX',
+    fees: '$600',
+    description: 'Huayna picchu is a.'
+  },
+
+  {
+    id:4,
+    imgSrc: img4,
+    destTitle: 'Cappadocia',
+    location: 'Turkey',
+    grade: 'CULTURAL RELAX',
+    fees: '$800',
+    description: 'Huayna picchu is a.'
+  },
+
+  {
+    id:5,
+    imgSrc: img5,
+    destTitle: 'Guanajuato',
+    location: 'Mexico',
+    grade: 'CULTURAL RELAX',
+    fees: '$11000',
+    description: 'Huayna picchu is a.'
+  },
+
+  {
+    id:6,
+    imgSrc: img6,
+    destTitle: 'Cinque Terre',
+    location: 'Italy',
+    grade: 'CULTURAL RELAX',
+    fees: '$840',
+    description: 'Huayna picchu is a.'
+  },
+
+  {
+    id:7,
+    imgSrc: img7,
+    destTitle: 'Angkor Wat',
+    location: 'Cambodia',
+    grade: 'CULTURAL RELAX',
+    fees: '$790',
+    description: 'Huayna picchu is a.'
+  },
+
+  {
+    id:8,
+    imgSrc: img8,
+    destTitle: 'Taj Mahal',
+    location: 'India',
+    grade: 'CULTURAL RELAX',
+    fees: '$900',
+    description: 'Huayna picchu is a.'
+  },
+
+  {
+    id:9,
+    imgSrc: img9,
+    destTitle: 'Bali Island',
+    location: 'Indonesia',
+    grade: 'CULTURAL RELAX',
+    fees: '$500',
+    description: 'Huayna picchu is a.'
+  }
+
+]
+
+const Main = () => {
+  useEffect(()=>{
+    Aos.init({duration: 2000})
+  },[])
+
+  return (
+    <section className='main container section'>
+      <div className="secTitle">
+        <h3 data-aos='fade-right' className='title'>
+          New Features Available
+        </h3>
+      </div>
+
+      <div className="secContent grid">
+        {
+          Data.map(({id,imgSrc,destTitle,location,grade,fees,description})=>{
+            return(
+              <div key={id} data-aos='fade-up' className="singleDestination">
+                {/*Here it will return single id fron the map array*/}
+
+                <div className="imageDiv">
+                  <img src={imgSrc} alt={destTitle}/>
+                </div>
+
+                <div className="cardInfo">
+                  <h4 className='destTitle'>{destTitle}</h4>
+                  <span className='continent flex'>
+                    <HiOutlineLocationMarker className='icon'/>
+                    <span className='name'>{location}</span>
+                  </span>
+
+                  <div className="fees flex">
+                    <div className='grade'>
+                      <span>{grade}<small>+1</small></span>
+                    </div>
+                    <div className="price">
+                      <h5>{fees}</h5>
+                    </div>
+                  </div>
+
+                  <div className="desc">
+                    <p>{description}</p>
+                  </div>
+
+                  <button className='btn flex'>
+                    DETAILS <HiOutlineClipboardCheck className='icon'/>
+                  </button>
+
+                </div>
+              </div>
+            )
+          })
+        }
+
+
+      </div>
+    </section>
+  )
+}
+
+export default Main
